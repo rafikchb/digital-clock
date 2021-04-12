e5ae3d629199856ed20a9b5946155efe059c831c
rafikchb <rafikchb@hotmail.fr>
2021-04-12 20:23:47 +0200

    CSS ADD

--- clock.js
+++ clock.js
@@ -1,12 +1,48 @@
-function displayTime(){
-    let clock = document.querySelector("#clock");
+function displayTime() {
+    let h = document.querySelector("#h");
+    let m = document.querySelector("#m");
+    let s = document.querySelector("#s");
+    let day = document.querySelector("#day");
     let now = new Date();
-    clock.textContent = now.toLocaleTimeString(); 
+    //clock.textContent = now.toLocaleTimeString(); 
+    h.textContent = now.getHours();
+    m.textContent = now.getMinutes();
+    s.textContent = now.getSeconds();
     
+
+    switch (now.getDay()) {
+        case 0:
+            // code block
+            day.textContent  = "Sunday"; 
+
+            break;
+        case 1:
+            // code block
+            day.textContent = "Monday";
+            break;
+        case 2:
+            // code block
+            day.textContent  = "Tuesday";
+            break;
+        case 3:
+            // code block
+            day.textContent  = "Wednesday";
+            break;
+        case 4:
+            // code block
+            day.textContent  = "Thursday";
+            break;
+        case 5:
+            // code block
+            day.textContent = "Friday";
+            break;
+        case 6:
+            // code block
+            day.textContent = "Saturday";
+            break;
+
+    }
 }
-function timesnpshot (){
-    return document.querySelector("#clock").textContent;
-}
-clock.addEventListener("click", timesnpshot); 
 displayTime(); 
+
 setInterval(displayTime, 1000);
